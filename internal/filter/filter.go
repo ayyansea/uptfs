6b5544dc75a8bc37427ef0a6e612b18bc677d79f
ayyansea <ayyansea@gmail.com>
2024-12-04 22:07:55 +0300

    fix: remove Greeter interface, why was it even there

--- internal/filter/filter.go
+++ internal/filter/filter.go
@@ -1,35 +1,13 @@
 package filter
 
-import (
-	"fmt"
-)
-
 type Filter struct {
 	name       string
 	action     func(string) string
 	subfilters []Filter
 }
 
-type Greeter interface {
-	Greet()
-}
-
 type Filterer interface {
-	Greeter
 	Filter(string) string
-}
-
-func (f Filter) Greet() {
-	subfilterCount := len(f.subfilters)
-
-	fmt.Printf("I am a filter and my name is %v\n", f.name)
-	if subfilterCount > 0 {
-		fmt.Println("My subfilters are:")
-
-		for _, subfilter := range f.subfilters {
-			fmt.Printf("- %v\n", subfilter.name)
-		}
-	}
 }
 
 func (f Filter) Filter(token string) (modifiedToken string) {
